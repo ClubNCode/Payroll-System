10f2829c27f0c3ac8a092edd65763ee3e31c862e
Rohit Raj <devrohitraj@users.noreply.github.com>
2016-03-14 15:58:48 +0530

    Update InvalidInputException.java
    Changes toString() with clear message.

--- payroll/util/InvalidInputException.java
+++ payroll/util/InvalidInputException.java
@@ -2,13 +2,10 @@ package com.raj.payroll.util;
 
 public class InvalidInputException extends Exception {
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public String toString(){
-		return "INVALID INPUT";
+		return "INVALID INPUT GIVEN";
 		
 	}
 
